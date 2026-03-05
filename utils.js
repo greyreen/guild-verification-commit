cd97756745ab36a271a5d1b992416ea8f8cb219f
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 20:59:22 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:59:19.458Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:59:22.216Z
+Commit: Fix race condition in async operations
