e9da98a20888071994031847405f574594739b03
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 20:57:13 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:56:57.806Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:57:12.782Z
+Commit: Fix broken links in documentation
