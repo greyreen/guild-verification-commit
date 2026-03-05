4e713ba62a754a3b70b69e7a58f9e0afd740d3e9
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 21:02:39 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:02:01.579Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:02:38.483Z
+Commit: Patch security vulnerability in authentication
