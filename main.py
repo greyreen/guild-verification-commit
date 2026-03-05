c7dc59067f18f80f991646be73cd4fdc845e5c1e
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 21:01:51 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:55:16.222Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T18:01:51.143Z
+Commit: Fix CSS styling bug on mobile devices
