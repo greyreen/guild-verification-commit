891235ef1b7590402ae008b5ece97f34d7bd9d75
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 21:01:19 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:01:13.373Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T18:01:19.254Z
+Commit: Fix broken links in documentation
