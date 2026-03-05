2ccdfe8448f0502aaec51c60ed1e1fdbc34425fe
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 21:02:02 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:01:47.574Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T18:02:01.579Z
+Commit: Add export functionality for reports
