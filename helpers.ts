04c1b8ce1b87be37c3f1c81c1622d71ebf757823
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 20:59:03 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:58:57.645Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T17:59:03.025Z
+Commit: Fix memory leak in event handlers
