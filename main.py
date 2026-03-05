0736db50e6ac8f34d507a456bc1016dcfa07722f
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 21:02:40 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:02:12.581Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T18:02:39.953Z
+Commit: Introduce real-time notifications
