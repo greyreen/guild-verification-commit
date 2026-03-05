5966f97a4f584b45cc8ddde59961b8231fe54410
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 20:59:26 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:57:12.782Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:59:25.942Z
+Commit: Fix memory leak in event handlers
