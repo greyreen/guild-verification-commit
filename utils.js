e21b676bf5c658e828a1a1f0e7b66b0243de5a7d
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-05 21:01:48 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:01:42.640Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T18:01:47.574Z
+Commit: Fix CSS styling bug on mobile devices
