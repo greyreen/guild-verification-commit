eaf9fa7dd6bc0ed1193dddd383535e96c531dcff
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-07 00:48:16 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:02:38.483Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T21:48:15.457Z
+Commit: Resolve null pointer exception in user service
