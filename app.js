c4b763175e26416814410c53650de7358fea5f2d
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-03-23 23:15:09 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-17T16:38:06.362Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-23T20:15:08.830Z
+Commit: Resolve null pointer exception in user service
