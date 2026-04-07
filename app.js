81cac141637b95f28294bf8c007887d2c19148ae
Osman Mutlu Işık <88304428+greyreen@users.noreply.github.com>
2026-04-07 07:37:02 +0300

    Introduce real-time notifications

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-23T20:15:08.830Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-07T04:37:02.032Z
+Commit: Introduce real-time notifications
